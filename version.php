a166f8d0cd4ec439f66080c3c8184d49ef72d34c
Chad Mazilly <chadmazilly@gmail.com>
2016-10-12 14:53:19 -0500

    bump ver

--- version.php
+++ version.php
@@ -23,6 +23,6 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016100600;
+$plugin->version   = 2016101200;
 $plugin->requires  = 2016051900;
 $plugin->component = 'gradereport_forecast';
